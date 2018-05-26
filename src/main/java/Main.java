ff9ca8cc8948094a5e072451fb0951159494e745
baby7305 <songhaibei7305@163.com>
2018-05-26 17:10:50 +0800

    测试字符串前面有0不影响数字转换

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        String test = "123";
+        int good = Integer.parseInt(test);
+        System.out.println(good);
+
+        String test1 = "00256";
+        int good1 = Integer.parseInt(test1);
+        System.out.println(good1);
     }
 }
