dce9e90f121dedc4748014fedbbaf2398813ad60
songhaibei <songhaibei@shmingjiang.com.cn>
2018-09-17 18:30:10 +0800

    对接测试FTP服务器

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,12 @@
+import FTP.FtpUtils;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        FtpUtils ftp = new FtpUtils();
+        //ftp.uploadFile("ftpFile/data", "123.docx", "E://123.docx");
+        //ftp.downloadFile("ftpFile/data", "123.docx", "F://");
+//        ftp.deleteFile("ftpFile/data", "123.docx");
+        ftp.downloadFile("ftpFile", "AA.png", "E://");
+        System.out.println("ok");
     }
 }
